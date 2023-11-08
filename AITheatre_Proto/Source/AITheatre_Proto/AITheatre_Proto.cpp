fdd27f5ba79c531bb6a64d951df9e29ef4aa38b6
Nils Meijer <meijernils9@gmail.com>
2023-11-08 13:47:28 +0100

    Started new project.  Created test level for raspberry.

--- AITheatre_Proto/Source/AITheatre_Proto/AITheatre_Proto.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AITheatre_Proto.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AITheatre_Proto, "AITheatre_Proto" );
