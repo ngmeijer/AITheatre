05e7d0d13f5a402d4759a56dbcfb38fbb7221ab8
Nils Meijer <meijernils9@gmail.com>
2023-11-17 13:49:31 +0100

    Started working on phone - server connection

---
+++ AItheatre-WearableApp/Assets/NetworkManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+
+public class NetworkManager : MonoBehaviour
+{
+    private TcpClient socket;
+    
+    private void Start()
+    {
+        Network
+    }
+}
---
+++ TCP server/server.py
@@ -0,0 +1,4 @@
+import socket
+import sys
+
+input("press ENTER to exit")
